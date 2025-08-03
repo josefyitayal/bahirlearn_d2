861c79c1ef1a5f554ba0387298974cdff194c1e9
yosef <josefyitayal@gmail.com>
2025-08-03 15:43:34 +0300

    install shadcn

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
+import { Button} from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div>
       <p className="text-red-700 text-3xl">Yosef</p>
+      <Button>click me</Button>
     </div>
   );
 }
